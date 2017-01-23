5c477b255f6233343e610013f7e076af6a5d10ac
Paul Mohabir <Paul.Mohabir88@gmail.com>
2017-01-23 17:37:10 -0500

    completed calculator will attempt bonuse after 4th problem

---
+++ 02_calculator/calculator.rb
@@ -0,0 +1,14 @@
+def add(x, y)
+  x+y
+end
+
+def subtract(x, y)
+  x-y
+end
+
+def sum(arr)
+  total = 0
+  #TOTAL = 0 TO START THEN ITERATE THROUGH THE NEW ARRAY AND ADD THE PREVIOUS VALUE
+  arr.each { |sum,arr| sum += x }
+  return total
+end
